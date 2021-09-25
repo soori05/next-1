1049ddc84c601f4a227373027c51de440f3852ef
Jayasurya H <86529451+soori05@users.noreply.github.com>
2021-09-25 23:09:02 +0530

    scrpt removr

--- pages/index.js
+++ pages/index.js
@@ -33,17 +33,7 @@ export default function Home() {
 
 
 
-    <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-
-<script
-  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-  crossorigin></script>
-
-<script
-  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-  crossorigin></script>
-
-<script>var Alert = ReactBootstrap.Alert;</script>
+    
     </main>
 
 
